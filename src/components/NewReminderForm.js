cd28ce4ab7238b20b9249bf051d0b978820d2721
dmmaia <david_mmaia@hotmail.com>
2020-09-27 00:50:22 -0300

    style has changed

--- src/components/NewReminderForm.js
+++ src/components/NewReminderForm.js
@@ -19,21 +19,23 @@ const NewReminderForm = () => {
   const [datePickerDisplay, setDatePickerDisplay] = useState(false);
 
   return (
-    <View>
-      <Text style={styles.labelForm}>Title</Text>
+    <View style={styles.boxNewReminder}>
+      <Text style={styles.labelFieldTitle}>Title:</Text>
       <TextInput
         value={title}
         onChange={(event) => {
           setTitle(event);
         }}
+        style={styles.boxFieldTitle}
       />
 
-      <Text style={styles.labelForm}>Description</Text>
+      <Text style={styles.labelForm}>Description:</Text>
       <TextInput
         value={description}
         onChange={(event) => {
           setDescription(event);
         }}
+        style={styles.boxField}
       />
 
       <Text style={styles.labelForm}>Date: </Text>
@@ -41,7 +43,7 @@ const NewReminderForm = () => {
       <Text style={styles.labelForm}>Hour: </Text>
 
       <TouchableOpacity style={styles.buttonStyle}>
-        <Text style={styles.buttonStyleText}>Add Reminder</Text>
+        <Text style={styles.buttonStyleText}>+Add Reminder</Text>
       </TouchableOpacity>
     </View>
   );
@@ -50,19 +52,42 @@ const NewReminderForm = () => {
 export default NewReminderForm;
 
 const styles = StyleSheet.create({
+  boxNewReminder: {
+    paddingBottom: 20,
+    borderRadius: 10,
+  },
+  boxFieldTitle: {
+    borderColor: '#909090',
+    borderBottomWidth: 2,
+    fontSize: 23,
+    marginBottom: 40,
+  },
+  boxField: {
+    borderColor: '#909090',
+    borderBottomWidth: 2,
+    fontSize: 20,
+  },
+  labelFieldTitle: {
+    fontSize: 26,
+    color: '#1B0C5D',
+    marginLeft: 10,
+  },
   labelForm: {
     fontSize: 20,
+    color: '#1B0C5D',
+    marginLeft: 10,
   },
   buttonStyle: {
     alignSelf: 'center',
-    width: 98 + '%',
+    width: 80 + '%',
     borderRadius: 5,
-    height: 35,
+    height: 40,
     backgroundColor: '#1BFA5D',
   },
   buttonStyleText: {
-    fontSize: 22,
+    fontSize: 23,
     alignSelf: 'center',
     color: '#1B0C5D',
+    fontWeight: 'bold',
   },
 });
